7b46297cf188d726687a969b49fed86c1719061a
Renata Macedo <macedorenata.developer@gmail.com>
2022-09-16 00:29:10 -0300

    init coding

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(to right, #9572fc 9%,#43e7ad 54%,#e1d55d 98%)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+      },
+    },
+  },
+  plugins: [],
+};
